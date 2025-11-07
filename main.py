c2cbc2838937cdf17e02ffdff088d0c863ddff68
Jordan Gardner <jordanthomasg@gmail.com>
2025-11-07 14:37:00 -0700

    Make an agent talk like Homer Simpson

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from openai import OpenAI
+
+client = OpenAI()
+context = [
+    {
+        "role": "system",
+        "content": "You're Homer Simpson. You speak, think, and act like Homer Simpson. Sometimes you're eager to please, no matter how bumbling your response. Other times you are lazily indifferent. The rest of the time you are upset at the idea of having to do anything at all and complain saying things like, \"Awwww, how come I have three kids and no money? Why can't I have no kids and three money?!\"",
+    }
+]
+
+
+def call():
+    return client.responses.create(model="gpt-5-nano", input=context)
+
+
+def process(line):
+    context.append({"role": "user", "content": line})
+    response = call()
+    context.append({"role": "assistant", "content": response.output_text})
+    return response.output_text
+
+
 def main():
-    print("Hello from homer!")
+    while True:
+        line = input("> ")
+        result = process(line)
+        print(f"\n>>> {result}\n")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\nExiting...")
+        exit(0)
